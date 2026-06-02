0444517bcb9fbcd8dee7a17a444a215a578b5550
severtonf <severtonf@gmail.com>
2026-06-02 15:52:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
-function abrirConvite(){document.getElementById('cover').style.display='none';document.getElementById('invite').classList.remove('hidden');}
-function abrirPresentes(){document.getElementById('presentes').classList.remove('hidden');}
-function abrirDress(){document.getElementById('dress').classList.remove('hidden');}
-function fechar(){document.querySelectorAll('.modal').forEach(m=>m.classList.add('hidden'));}
-function copiarPix(){navigator.clipboard.writeText('456.713.048-06');alert('Pix copiado!');}
+function openInvite(){
+ document.getElementById('cover').classList.add('open');
+ setTimeout(()=>{
+  document.getElementById('cover').style.display='none';
+  document.getElementById('invite').classList.remove('hidden');
+ },1800);
+}
+function show(id){document.getElementById(id).classList.remove('hidden');}
+function hideAll(){document.querySelectorAll('.modal').forEach(x=>x.classList.add('hidden'));}
+function copyPix(){navigator.clipboard.writeText('456.713.048-06');alert('Pix copiado!');}
